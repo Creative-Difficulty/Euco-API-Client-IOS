8ca87a69ec4e16eae826b4ef7d00332530a97cdf
Creative-Difficulty <alexleschanz@icloud.com>
2022-07-16 20:02:11 +0200

    Initial Commit

---
+++ Shared/Euco_API_Client_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Euco_API_Client_IOSApp.swift
+//  Shared
+//
+//  Created by Alexander Leschanz on 16.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Euco_API_Client_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
