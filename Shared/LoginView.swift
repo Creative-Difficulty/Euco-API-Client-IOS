97cb15e2ceb70df96f9c22d48680c145469f1d9d
Creative-Difficulty <alexleschanz@icloud.com>
2022-07-20 12:11:05 +0200

    implement proper Token verification

--- Shared/LoginView.swift
+++ Shared/LoginView.swift
@@ -49,7 +49,7 @@ struct LoginView: View {
                     .border(.red, width: CGFloat(wrongToken))
                 
                 Button("Authenticate") {
-                    authenticateUser(username: EucoAPIURL, password: EucoAPIToken)
+                    authenticateUser(EucoAPIURL: EucoAPIURL, EucoAPIToken: EucoAPIToken)
                     
                 }
                 .foregroundColor(.white)
@@ -69,22 +69,51 @@ struct LoginView: View {
 //        }
 }
 
-func authenticateUser(username: String, password: String) {
-    let url = URL(string: username)
-    var request = URLRequest(url: url!)
-    request.setValue(password, forHTTPHeaderField: "authentication")
+func authenticateUser(EucoAPIURL: String, EucoAPIToken: String) {
+    wrongToken = 0
+    wrongURL = 0
+    
+    guard let url = URL(string: EucoAPIURL) else {
+        wrongURL = 1
+        return
+        
+    }
+    
+    var request = URLRequest(url: url)
+    request.setValue(EucoAPIToken, forHTTPHeaderField: "authentication")
     URLSession.shared.dataTask(with: request) { (data, response, error) in
+        if error != nil {
+            wrongURL = 1
+            return
+        }
         if let data = data {
+            wrongURL = 0
             if let base64Data = String(data: data, encoding: .utf8) {
+                if Data(base64Encoded: base64Data) == nil {
+                    wrongToken = 1
+                    return
+                }
                 let decodedData = Data(base64Encoded: base64Data)!
                 let decodedString = String(data: decodedData, encoding: .utf8)!
-                print(decodedString)
-                loggedIn = true
+                let JSON: EucoAPIJSON = try! JSONDecoder().decode(EucoAPIJSON.self, from: Data(decodedString.utf8))
+                
+                if JSON.success == true {
+                    if let httpResponse = response as? HTTPURLResponse {
+                        if httpResponse.statusCode == 200 {
+                            print(decodedString)
+                            loggedIn = true
+                        }
+                    }
+                }
             }
         }
         
     }.resume()
 }
+}
+
+struct EucoAPIJSON: Decodable {
+    let success: Bool
 }
 
 struct LoginView_Previews: PreviewProvider {
